1873d7b8bb6718ba34a4d7ca81134e186461846d
Matthew Phillips <matthew@matthewphillips.info>
2019-02-07 08:16:13 -0500

    site

---
+++ precache-manifest.968bfcfbf2ca9c5640b9b14f3155c69c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4efdda328ccb8e2540db27e57aaf1cc4",
+    "url": "./static/media/shopping-cart.4efdda32.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a4ae9460ef3453ee297d",
+    "url": "./static/js/main.a4ae9460.chunk.js"
+  },
+  {
+    "revision": "84260016e6b29a7c4cac",
+    "url": "./static/js/1.84260016.chunk.js"
+  },
+  {
+    "revision": "a4ae9460ef3453ee297d",
+    "url": "./static/css/main.1bbe9ab8.chunk.css"
+  },
+  {
+    "revision": "94c3ce9a5ec55a2b2f4c8d9410bf100c",
+    "url": "./index.html"
+  }
+];
